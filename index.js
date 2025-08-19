4a4226f484e4c0cf34275eddd2c94deef16de542
Hxcker-Obama <126404326+Hxcker-Obama@users.noreply.github.com>
2025-08-19 21:06:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const { Client } = require('discord.js-selfbot-v13');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Bot is alive!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 const client = new Client();
 
